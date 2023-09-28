1b07f937cdf3aa67b9b6c0e5135736514d06ed81
Kiana Liaghat <k.liaghat2000@gmail.com>
2023-09-28 14:30:34 +0330

    Update nn_t.vhd

--- vhdl-code/nn_t.vhd
+++ vhdl-code/nn_t.vhd
@@ -1,30 +1,7 @@
 --------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   12:40:05 01/30/2022
--- Design Name:   
--- Module Name:   F:/Term 7/FPGA/project/NN/QNN/nn_t.vhd
--- Project Name:  QNN
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: nn
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
+-- This is the test file for nn.vhd
 --------------------------------------------------------------------------------
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
